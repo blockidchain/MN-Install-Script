46f357b6c65c9080367b4f732278984851203232
mdfkbtc <43780846+mdfkbtc@users.noreply.github.com>
2021-05-05 09:10:23 +0200

    Fix missing packages for minimalistic ubuntu

--- masternode-install-ubuntu-18.sh
+++ masternode-install-ubuntu-18.sh
@@ -223,6 +223,7 @@ fi
 function prepare_system() {
 echo -e "Preparing the VPS to setup. ${CYAN}$COIN_NAME${NC} ${RED}Masternode${NC}"
 apt-get update >/dev/null 2>&1
+apt-get install lsb-core >/dev/null 2>&1
 DEBIAN_FRONTEND=noninteractive apt-get update > /dev/null 2>&1
 DEBIAN_FRONTEND=noninteractive apt-get -o Dpkg::Options::="--force-confdef" -o Dpkg::Options::="--force-confold" -y -qq upgrade >/dev/null 2>&1
 apt install -y software-properties-common >/dev/null 2>&1
